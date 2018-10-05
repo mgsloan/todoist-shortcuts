7ed6f26926e6a0fa1718397b5a19b361a35778ba
Michael Sloan <mgsloan@gmail.com>
2018-10-05 13:56:43 -0700

    Version 25

--- src/todoist-shortcuts.js
+++ src/todoist-shortcuts.js
@@ -6,7 +6,7 @@
 // @include     http://todoist.com/app*
 // @include     https://beta.todoist.com/app*
 // @include     http://beta.todoist.com/app*
-// @version     24
+// @version     25
 // @grant       none
 // ==/UserScript==
 
@@ -18,7 +18,7 @@
 (function() {
   'use strict';
 
-  var TODOIST_SHORTCUTS_VERSION = 24;
+  var TODOIST_SHORTCUTS_VERSION = 25;
 
   // When true, enables selecting multiple items by holding 'x' and moving the
   // cursor.
