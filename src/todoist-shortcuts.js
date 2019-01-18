3271fc913297c92bcb6529c01e34a191b0d3180c
Michael Sloan <mgsloan@gmail.com>
2019-01-17 23:35:15 -0800

    Bump to Todoist version 935

--- src/todoist-shortcuts.js
+++ src/todoist-shortcuts.js
@@ -354,7 +354,7 @@
   var TODOIST_SHORTCUTS_NAVIGATE = 'todoist_shortcuts_navigate';
 
   var TODOIST_SHORTCUTS_GITHUB = 'https://github.com/mgsloan/todoist-shortcuts';
-  var TODOIST_TESTED_VERSION = 931;
+  var TODOIST_TESTED_VERSION = 935;
 
   // This user script will get run on iframes and other todoist pages. Should
   // skip running anything if #todoist_app doesn't exist.
