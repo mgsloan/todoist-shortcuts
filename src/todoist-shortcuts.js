76017953075ea4f4a2d089eb9a7692f7bcdbde66
Michael Sloan <mgsloan@gmail.com>
2019-05-27 21:53:01 -0600

    Version 51
    I fumble fingered a mozilla extension upload, which appears to have
    made a version number incacessible, which is dumb.

--- src/todoist-shortcuts.js
+++ src/todoist-shortcuts.js
@@ -6,7 +6,7 @@
 // @include     http://todoist.com/app*
 // @include     https://beta.todoist.com/app*
 // @include     http://beta.todoist.com/app*
-// @version     50
+// @version     51
 // @grant       none
 // ==/UserScript==
 
@@ -18,7 +18,7 @@
 (function() {
   'use strict';
 
-  var TODOIST_SHORTCUTS_VERSION = 50;
+  var TODOIST_SHORTCUTS_VERSION = 51;
 
   // Set this to true to get more log output.
   var DEBUG = false;
