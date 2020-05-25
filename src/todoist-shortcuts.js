f27ec643d7797220132260069bf7ec4ed0078931
Michael Sloan <mgsloan@gmail.com>
2020-05-25 15:59:23 -0600

    Version 84

--- src/todoist-shortcuts.js
+++ src/todoist-shortcuts.js
@@ -6,7 +6,7 @@
 // @include     http://todoist.com/app*
 // @include     https://beta.todoist.com/app*
 // @include     http://beta.todoist.com/app*
-// @version     83
+// @version     84
 // @grant       none
 // ==/UserScript==
 
@@ -18,7 +18,7 @@
 (function() {
   'use strict';
 
-  var TODOIST_SHORTCUTS_VERSION = 83;
+  var TODOIST_SHORTCUTS_VERSION = 84;
 
   // Set this to true to get more log output.
   var DEBUG = false;
