dbd71b372f30c44ed246d376f9f5aed82c8f7dac
Michael Sloan <mgsloan@gmail.com>
2020-06-13 23:20:12 -0600

    Update inject script

--- src/inject.js
+++ src/inject.js
@@ -1,18 +1,6 @@
-// Via https://gist.github.com/nok/a98c7c5ce0d0803b42da50c4b901ef84
+// Injects the main script. This is necessary in order to bind a
+// keyhandler.
 
-/**
- * injectScript - Inject internal script to available access to the `window`
- *
- * @param  {type} file_path Local path of the internal script.
- * @param  {type} tag The tag as string, where the script will be append (default: 'body').
- * @see    {@link http://stackoverflow.com/questions/20499994/access-window-variable-from-content-script}
- */
-function injectScript(file_path, tag) {
-    var node = document.getElementsByTagName(tag)[0];
-    var script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src', file_path);
-    node.appendChild(script);
-}
-
-injectScript(chrome.extension.getURL('todoist-shortcuts.js'), 'body');
+const scriptEl = document.createElement('script');
+scriptEl.setAttribute('src', chrome.extension.getURL('todoist-shortcuts.js'));
+document.getElementsByTagName('body')[0].appendChild(scriptEl)
