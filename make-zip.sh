6567f4672d3b9ba52ee42085161dde61150d75ac
Michael Sloan <mgsloan@gmail.com>
2020-06-13 23:14:52 -0600

    Remove some old comments in the scripts

--- make-zip.sh
+++ make-zip.sh
@@ -7,9 +7,3 @@ rm -f todoist-shortcuts.zip
 cd src
 zip ../todoist-shortcuts.zip inject.js manifest.json todoist-shortcuts.js icon32.png icon48.png icon64.png icon128.png
 cd ../
-
-# Turns out chrome webstore just wants a zip
-#
-# rm -f src.pem src.crx
-# google-chrome --pack-extension=src --pack-extension-key=private/todoist-shortcuts.pem
-# mv src.crx todoist-shortcuts.crx
