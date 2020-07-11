9b0d057810c8e8820b91b064640a130b3f767471
Michael Sloan <mgsloan@gmail.com>
2020-07-10 23:58:36 -0600

    Version 93

--- src/todoist-shortcuts.js
+++ src/todoist-shortcuts.js
@@ -5,7 +5,7 @@
 /* global ItemSelecter, svgs */
 
 {
-  var TODOIST_SHORTCUTS_VERSION = 92;
+  var TODOIST_SHORTCUTS_VERSION = 93;
 
   // Set this to true to get more log output.
   var DEBUG = false;
