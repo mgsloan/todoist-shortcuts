eb8382ac69cfd898d643f2487f5461b171cbd2b5
Michael Sloan <mgsloan@gmail.com>
2022-02-06 11:06:11 -0700

    Copy mousetrap.js into src
    Firefox doesn't seem to support loading scripts from web accessible
    resources in unpacked extensions, when the scripts are symlinks

--- eslint.sh
+++ eslint.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 ./node_modules/eslint/bin/eslint.js src/todoist-shortcuts.js $@
+
+CMP_STATUS="$(cmp --silent -- src/mousetrap.js mousetrap/mousetrap.js; echo $?)"
+if [[ "$CMP_STATUS" -ne 0 ]]; then
+  echo "ERROR: src/mousetrap.js and mousetrap/mousetrap.js should have same content."
+  exit 1
+fi
